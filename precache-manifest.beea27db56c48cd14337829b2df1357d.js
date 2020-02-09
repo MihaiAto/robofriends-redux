56f7de13be0977b7c23c7d5f19fc4c6e1c22541a
Mihai Atofanei <mihai.atofanei@gmail.com>
2020-02-10 10:59:35 +1100

    Updates

---
+++ precache-manifest.beea27db56c48cd14337829b2df1357d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4287df98b8456404685",
+    "url": "/robofiends-redux/static/css/main.4bacb053.chunk.css"
+  },
+  {
+    "revision": "d4287df98b8456404685",
+    "url": "/robofiends-redux/static/js/main.d4287df9.chunk.js"
+  },
+  {
+    "revision": "b90b6eed5e45908cab57",
+    "url": "/robofiends-redux/static/css/1.e9863852.chunk.css"
+  },
+  {
+    "revision": "b90b6eed5e45908cab57",
+    "url": "/robofiends-redux/static/js/1.b90b6eed.chunk.js"
+  },
+  {
+    "revision": "405c38001d1630c13bdf",
+    "url": "/robofiends-redux/static/js/runtime~main.405c3800.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofiends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "bd03759098818cba2e1df2163f04c495",
+    "url": "/robofiends-redux/index.html"
+  }
+];
